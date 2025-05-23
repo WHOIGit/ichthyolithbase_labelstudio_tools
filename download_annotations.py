7ae7f20b01462d1a2f28f4e1103f72bcd05eb922
Sidney Batchelder <44208509+sbatchelder@users.noreply.github.com>
2025-05-23 11:22:43 -0400

    moved Z1 to numerical_annot_fields

--- download_annotations.py
+++ download_annotations.py
@@ -142,13 +142,13 @@ def download_snap_as_df(ls, project_id, snap_id):
 def cleanup_df(df):
     print('Cleaning-up dataframe')
     # Define fields
-    numerical_annot_fields = [
+    numerical_annot_fields = ['Z1',
        'A1', 'A2', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3', 'D1', 
        'E1', 'E2', 'E3', 'E4', 'F1', 'G1', 'G2', 'G3', 'H1',
        'I1', 'I2', 'I3', 'J1', 'J2', 'K1', 'K2', 'L1', 'L2', 'L3',
        'L4', 'M1', 'M2', 'M3', 'M4', 'N1', 'N2', 'N3',
        'O1', 'O2', 'O3', 'O4', 'O5', 'O6', 'O7', 'O8', 'O9', 'O10']
-    extra_annot_fields = ['Z1','Type']
+    extra_annot_fields = ['Type']
     cache_fields = ['cache_'+col for col in extra_annot_fields+numerical_annot_fields]
     data_fields = ['pid', 'site', 'slide', 'sediment', 'iodp', 'fraction', 
                    'Slide Hole', 'Slide Hole Max', 'obj', 'magnification',
